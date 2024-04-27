a79293dfbc865653e5bc59b35e5e78a8920fed23
ShuoxiTheInari <shuoxi_thefox@outlook.com>
2024-04-28 01:05:46 +0800

    fix range error

--- utils/cost.py
+++ utils/cost.py
@@ -1,40 +1,23 @@
-import math
 
 def expaction(p,n,P_x,type):
     if type == "mi":
-        E = sum([i*p*(1-p)**(i-1) for i in range(1,n-1)]) + P_x*n*(1-p)**(n-1) + (1-P_x)*(1-p)**(n-1)*(sum([(i)*p*(1-p)**(i-2) for i in range(n+1,2*n-2)]) + 2*n*(1-p)**(2*n-2))
+        E = sum([i*p*(1-p)**(i-1) for i in range(1,n)]) + P_x*n*(1-p)**(n-1) + (1-P_x)*(1-p)**(n-1)*(sum([(i)*p*(1-p)**(i-2) for i in range(n+1,2*n-1)]) + 2*n*(1-p)**(2*n-2))
     
     elif type == "single":
-        E = sum([i*p*(1-p)**(i-1) for i in range(1,n-1)]) + n*(1-p)**(n-1)
+        E = sum([i*p*(1-p)**(i-1) for i in range(1,n)]) + n*(1-p)**(n-1)
     
     elif type == "none":
         E = 1/p
-    
     E= round(E,3)
     return E
-    
-    
-def max_cost(p,n,cost,P_x,type):
-    if type=="mi":
-        p_worst =(1-P_x)*((1-p)**(2*n-2))
-        cost_max = 2*n*cost
-    if type == "single":
-        p_worst = math.pow((1-p),n-1)
-        cost_max = n*cost
-    
-    return cost_max,p_worst
-
 def max_p_worst(p,n,P_x,type):
     if type=="mi":
         p_worst =(1-P_x)*((1-p)**(2*n-2))
         p_worst = round(p_worst,3)
 
     elif type == "single":
-        p_worst = math.pow((1-p),n-1)
+        p_worst = (1-p)**(n-1)
         p_worst = round(p_worst,3)
     else:
-        p_worst="-"
-    
-    
-    
+        p_worst="-"   
     return p_worst
